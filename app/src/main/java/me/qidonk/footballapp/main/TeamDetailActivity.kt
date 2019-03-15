132d0b94c996aa486ba7cc2790eafbb349cd658a
Miftahul Rizki P <mrizkip@student.ub.ac.id>
2019-03-16 06:55:14 +0700

    team detail on progress

--- app/src/main/java/me/qidonk/footballapp/main/TeamDetailActivity.kt
+++ app/src/main/java/me/qidonk/footballapp/main/TeamDetailActivity.kt
@@ -2,9 +2,17 @@ package me.qidonk.footballapp.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import me.qidonk.footballapp.R
+import me.qidonk.footballapp.model.Team
+import me.qidonk.footballapp.presenter.TeamPresenter
 
 class TeamDetailActivity : AppCompatActivity() {
+
+    private var team: Team? = null
+    private lateinit var presemter: TeamPresenter
+    private var menuItem: Menu? = null
+    private var isFavorite: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
