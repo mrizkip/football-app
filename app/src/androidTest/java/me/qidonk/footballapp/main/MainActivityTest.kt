ce83cc05349e2b5828c15ef2b57104a5cc92cfc7
mrizkip <mrizkip@hotmail.com>
2019-04-01 15:54:53 +0700

    fix Intrumentation test

--- app/src/androidTest/java/me/qidonk/footballapp/main/MainActivityTest.kt
+++ app/src/androidTest/java/me/qidonk/footballapp/main/MainActivityTest.kt
@@ -1,12 +1,12 @@
 package me.qidonk.footballapp.main
 
-import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.pressBack
 import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.contrib.RecyclerViewActions
 import android.support.test.espresso.matcher.ViewMatchers
-import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.support.v7.widget.RecyclerView
@@ -24,52 +24,58 @@ class MainActivityTest {
     @Test
     fun testRecyclerViewBehaviour() {
         delay()
-        onView(withId(R.id.lastMatch_recyclerView))
-            .check(matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.nextMatch_recyclerView))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         delay()
-        onView(withId(R.id.lastMatch_recyclerView))
-            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(10))
+        Espresso.onView(ViewMatchers.withId(R.id.nextMatch_recyclerView))
+                .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(10))
         delay()
-        onView(withId(R.id.lastMatch_recyclerView))
-            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(10, click()))
+        Espresso.onView(ViewMatchers.withId(R.id.nextMatch_recyclerView))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(10, click()))
     }
 
     @Test
     fun testAppBehaviour() {
         delay()
-        onView(withId(R.id.lastMatch_recyclerView))
-            .check(matches(isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.nextMatch_recyclerView))
+                .check(ViewAssertions.matches(isDisplayed()))
         delay()
-        onView(withId(R.id.lastMatch_recyclerView))
-            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(10))
+        Espresso.onView(ViewMatchers.withId(R.id.nextMatch_recyclerView))
+                .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(10))
         delay()
-        onView(withId(R.id.lastMatch_recyclerView))
-            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(10, click()))
+        Espresso.onView(ViewMatchers.withId(R.id.nextMatch_recyclerView))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(10, click()))
+        delay(2)
+        Espresso.onView(ViewMatchers.withId(R.id.add_to_favorite))
+                .check(ViewAssertions.matches(isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.add_to_favorite))
+                .perform(click())
+        Espresso.onView(ViewMatchers.withText("Added to favorite"))
+                .check(ViewAssertions.matches(isDisplayed()))
         delay()
-        onView(withId(R.id.add_to_favorite))
-            .check(matches(isDisplayed()))
-        delay()
-        onView(withId(R.id.add_to_favorite))
-            .perform(click())
-        delay()
-        onView(withText("Added to favorite"))
-            .check(matches(isDisplayed()))
-        delay()
-        onView(withId(R.id.add_to_favorite))
-            .perform(click())
-        delay()
-        onView(withText("Removed from favorite"))
-            .check(matches(isDisplayed()))
         pressBack()
-        onView(withId(R.id.main_bottomNavigation))
-            .check(matches(isDisplayed()))
-        onView(withId(R.id.menu_favorites))
-            .perform(click())
+        Espresso.onView(ViewMatchers.withId(R.id.main_bottomNavigation))
+                .check(ViewAssertions.matches(isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.menu_favorites))
+                .perform(click())
+        delay()
+        Espresso.onView(ViewMatchers.withId(R.id.favoritematch_recyclerView))
+                .check(ViewAssertions.matches(isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.favoritematch_recyclerView))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        delay(2)
+        Espresso.onView(ViewMatchers.withId(R.id.add_to_favorite))
+                .check(ViewAssertions.matches(isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.add_to_favorite))
+                .perform(click())
+        Espresso.onView(ViewMatchers.withText("Removed from favorite"))
+                .check(ViewAssertions.matches(isDisplayed()))
+        pressBack()
     }
 
-    private fun delay() {
+    private fun delay(second: Long = 1) {
         try {
-            Thread.sleep(1000)
+            Thread.sleep(1000 * second)
         } catch (e: InterruptedException) {
             e.printStackTrace()
         }
